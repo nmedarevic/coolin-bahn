fb56e5b00d280e54d1fcda6307d263d311a4dcd5
Nikola Medarevic <nikola@petmediagroup.com>
2024-02-04 21:01:25 +0100

    Cleanup

--- backend_ubahn/src/data/data-calculator.ts
+++ backend_ubahn/src/data/data-calculator.ts
@@ -1,32 +1,6 @@
-import { LineType } from "../domain/LineType";
+import { Station } from "../domain/Station";
 import { UBahn } from "./UBahn";
 import lines from "./lines.json";
-
-type LineConnection = {
-  lineName: string;
-  connectsTo: string[];
-}
-const connections: LineConnection[] = []
-
-console.log('\n\n', lines, '\n\n');
-
-export type Station = {
-  name: string;
-  lines: string[];
-}
-
-type Line = {
-  /** name of the line, can be used as an ID */
-  name: string;
-  /** the brand color to display the line with */
-  color: string;
-  /** ignore this for now */
-  type: LineType;
-  /** the stations corresponding to that line */
-  stations: Station[];
-}
-
-const linesMap: { [name: string]: Line } = {}
 
 const ubahn = new UBahn();
 
