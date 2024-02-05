30675d1540964ebd3c6eb6458033b98f6c15642b
Nikola Medarevic <nikola@petmediagroup.com>
2024-02-05 22:53:36 +0100

    Update line routes endpoint

--- backend_ubahn/src/routes/lineRoutes.ts
+++ backend_ubahn/src/routes/lineRoutes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { lines } from "../data";
+import { Direction } from "../domain/Direction";
+import {ubahn} from "../services/ubahn.service";
 
 const router = express.Router();
 
@@ -40,6 +42,48 @@ router.get(
     }
 
     res.send(requestedLine);
+  }
+);
+
+router.get(
+  "/:id/:station/:direction/:numberOfStations?",
+  /**
+   * returns a specific line by id, e.g. `GET /lines/U8`
+   */
+  async function getLineById(req, res) {
+    // find the specific line by key
+    const requestedLineId = req.params.id;
+    const requestedStation = req.params.station;
+    const requestedDirection = req.params.direction;
+    const requestedNumberOfStations = req.params.numberOfStations ? Number(req.params.numberOfStations) : 3;
+    const realName = ubahn.stationsUrlToName[requestedStation];
+    const requestedLine = lines.find((line) => line.name === requestedLineId);
+
+    if (!requestedLine) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const stationIndex = requestedLine.stations.findIndex((station) => station === realName);  
+
+    let stations: string[] = []
+    
+    if (requestedDirection === Direction.Forward) {
+      stations = requestedLine.stations.slice(stationIndex, stationIndex + requestedNumberOfStations);
+    }
+
+    if (requestedDirection === Direction.Backward) {
+      const startOfSlice = stationIndex - requestedNumberOfStations < 0 ? 0 : stationIndex - requestedNumberOfStations
+
+      stations = requestedLine.stations.slice(startOfSlice, stationIndex);
+    }
+
+    const response = {
+      ...requestedLine,
+      stations
+    }
+
+    res.send(response);
   }
 );
 
