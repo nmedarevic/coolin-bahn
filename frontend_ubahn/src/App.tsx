112b593cab548bbe96a1ac20557cf36f3faec6c5
Nikola Medarevic <nikola@petmediagroup.com>
2024-02-06 23:39:44 +0100

    [FE] Adds styles to the app component

--- frontend_ubahn/src/App.tsx
+++ frontend_ubahn/src/App.tsx
@@ -8,10 +8,26 @@ import { Stations } from "./components/Stations/Stations";
 const backend = "http://localhost:8080/"
 const linesUrl = `${backend}lines`
 
+export type Station = {
+  name: string;
+  lines: string[];
+}
+
+export type StationWithConnections = { 
+  name: string; 
+  connections: { name: string; lines: string[] }[] 
+}
+
+export type LinesWithStations = { 
+  [lineName: string]: StationWithConnections[]
+}
+
 function App() {
   const [lines, setLines] = useState<Line[]>([]);
   const [currentLine, setCurrentLine] = useState<string | null>(null);
-  const [stations, setStations] = useState<{[lineName: string]: string[]}>({});
+  const [currentStation, setCurrentStation] = useState<string | null>(null);
+  const [linesWithStations, setlinesWithStations] = useState<LinesWithStations>({});
+  
   useEffect(() => {
     async function fetchLines() {
       const response = await axios.get(linesUrl);
@@ -21,17 +37,40 @@ function App() {
     fetchLines();
   }, []);
 
-  const onStationSelect = async (lineName: string) => {
+  const onLineSelect = async (lineName: string) => {
     setCurrentLine(lineName)
+    setCurrentStation(null)
     const response = await axios.get(`${linesUrl}/${lineName}/stations`);
-    setStations({...stations, [lineName]: response.data});
+    setlinesWithStations({ ...linesWithStations, [lineName]: response.data });
   }
 
+  const onStationSelect = async (stationName: string) => {
+    setCurrentStation(stationName)
+  }
+
+  const focusedStation = currentStation && currentLine ? linesWithStations[currentLine].find((station: StationWithConnections) => station.name === currentStation) : null
+console.log('\n\n', focusedStation, '\n\n');
   return (
     <div className="App">
       <header className="App-header">
-        <Lines currentLine={currentLine} lines={lines} onStationSelect={onStationSelect}/>
-        {currentLine && stations[currentLine] ? <Stations stations={stations[currentLine]} /> : null}
+        <h1>Berlin U-Bahn lines</h1>
+        <Lines currentLine={currentLine} lines={lines} onLineSelect={onLineSelect} />
+        <div className="content">
+        {currentLine && linesWithStations[currentLine] ? <Stations stations={linesWithStations[currentLine]} onStationSelect={onStationSelect} /> : null}
+        {focusedStation ? <div className="lines">
+          <div><Lines lines={(focusedStation?.connections?.reduce((acc: string[], conn: Station) => {
+            const lines = conn.lines;
+
+            const uniqueLines = lines.filter((line) => !acc.includes(line))
+            
+            return [...acc, ...uniqueLines]
+          }, []).map((line) => ({
+            name: line,
+            color: lines.find((ln: Line) => ln.name === line)?.color,  
+          })) || []) as Line[]}/></div>
+        </div> : null}
+        <div className="">{focusedStation?.name}</div>
+        </div>
       </header>
     </div>
   );
