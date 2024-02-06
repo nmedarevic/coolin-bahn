47664774c105975ac4cfb7030066dffb283195ed
Nikola Medarevic <nikola@petmediagroup.com>
2024-02-06 23:40:24 +0100

    [FE] Updates Stations component

--- frontend_ubahn/src/components/Stations/Stations.tsx
+++ frontend_ubahn/src/components/Stations/Stations.tsx
@@ -1,10 +1,16 @@
+import { StationWithConnections } from "../../App";
+import styles from './Stations.module.scss';
+
 export const Stations = ({
-  stations
-}: {stations: string[]}) => {
+  stations,
+  onStationSelect
+}: {stations: StationWithConnections[]; onStationSelect: (stationName: string) => void}) => {
   console.log('\n\n', stations, '\n\n');
   return (
-    <>
-    {stations.map((station) => <div key={station}>{station}</div>)}
-    </>
+    <div className={styles.stationsWrapper}>
+    {stations.map((station) => <div key={station.name} onClick={() => {
+      onStationSelect(station.name);
+    }}>{station.name}</div>)}
+    </div>
   )
 }
